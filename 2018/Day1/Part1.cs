13adf0814a91f58f357aae080b845906451b7df0
Mike McDermott <mmcdermott@rightpoint.com>
2018-12-01 14:49:14 -0600

    Day 1 Part 1 solved

--- 2018/Day1/Part1.cs
+++ 2018/Day1/Part1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace AoC._2018.Day1
 {
@@ -8,8 +9,16 @@ namespace AoC._2018.Day1
 
         public void Solve()
         {
+            var input = RawInput.Split("\r\n");
+            var result = 0;
+
+            foreach(var freqShift in input)
+            {
+                result = result + int.Parse(freqShift, NumberStyles.AllowLeadingSign);
+            }
+
             Console.WriteLine("Day 1, Part 1");
-            Console.WriteLine(RawInput);
+            Console.WriteLine(result);
         }
     }
 }
