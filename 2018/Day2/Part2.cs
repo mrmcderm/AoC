8a01df849b1db33c0b70857d51a33e4eef2d0625
Mike McDermott <mmcdermott@rightpoint.com>
2018-12-01 23:35:48 -0600

    Day 2 Part 2 complete

--- 2018/Day2/Part2.cs
+++ 2018/Day2/Part2.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC._2018.Day2
 {
@@ -8,10 +10,46 @@ namespace AoC._2018.Day2
 
         public void Solve()
         {
+            Console.WriteLine("Day 2, Part 2");
+
             var result = RawInput;
 
-            Console.WriteLine("Day 2, Part 2");
-            Console.WriteLine(result);
+            var ids = result.Split("\r\n");
+
+            var comparisons = new List<Comparison>();
+            for (var i = 0; i < ids.Length; i++)
+            {
+                for (var j = 0; j < ids.Length; j++)
+                {
+                    //Don't compare to self
+                    if (i == j)
+                        continue;
+
+                    var delta = Delta(ids[i], ids[j]);
+                    comparisons.Add(new Comparison { Id1 = ids[i], Id2 = ids[j], Delta = delta });
+
+                }
+            }
+
+            foreach (var comparison in comparisons.Where(_ => _.Delta == 1))
+            {
+                Console.WriteLine(comparison.Id1);
+                Console.WriteLine(comparison.Id2);
+            }
         }
+
+        private int Delta(string firstId, string secondId)
+        {
+            return firstId.Where((t, i) => t != secondId[i]).Count();
+        }
+    }
+
+    public class Comparison
+    {
+        public string Id1 { get; set; }
+
+        public string Id2 { get; set; }
+
+        public int Delta { get; set; }
     }
 }
