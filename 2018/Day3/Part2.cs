a2c1dfe6c7700e6b4d805f405937be5f58301e14
Mike McDermott <mmcdermott@rightpoint.com>
2018-12-03 00:14:25 -0600

    Day 3, Part 2 solved

--- 2018/Day3/Part2.cs
+++ 2018/Day3/Part2.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC._2018.Day3
 {
@@ -8,10 +10,87 @@ namespace AoC._2018.Day3
 
         public void Solve()
         {
-            var result = RawInput;
-
             Console.WriteLine("Day 3, Part 2");
-            Console.WriteLine(result);
+
+            var rawClaims = RawInput.Split("\r\n");
+
+            //Parse the claims
+            var claims = new List<Claim>();
+            foreach (var rawClaim in rawClaims)
+            {
+                var claim = new Claim();
+                var atSplit = rawClaim.Split("@");
+                var colonSplit = atSplit[1].Trim().Split(":");
+                var commaSplit = colonSplit[0].Split(",");
+                var dimensionSplit = colonSplit[1].Trim().Split("x");
+
+                claim.Id = atSplit[0].Trim();
+                claim.LeftOffset = int.Parse(commaSplit[0]);
+                claim.TopOffset = int.Parse(commaSplit[1]);
+                claim.Width = int.Parse(dimensionSplit[0]);
+                claim.Height = int.Parse(dimensionSplit[1]);
+
+                claims.Add(claim);
+            }
+
+            var fabricWidth = 1000;
+            var fabricHeight = 1000;
+
+            var grid = new List<Claim>[fabricWidth, fabricHeight];
+
+            for (var i = 0; i < fabricWidth; i++)
+            {
+                for (var j = 0; j < fabricHeight; j++)
+                {
+                    grid[i, j] = new List<Claim>();
+                }
+            }
+
+            //Map the claims
+            foreach (var claim in claims)
+            {
+                for (var column = claim.LeftOffset; column < claim.LeftOffset + claim.Width; column++)
+                {
+                    for (var row = claim.TopOffset; row < claim.TopOffset + claim.Height; row++)
+                    {
+                        grid[column, row].Add(claim);
+                    }
+                }
+            }
+
+            /*
+                ........
+                ...2222.
+                ...2222.
+                .11XX22.
+                .11XX22.
+                .111133.
+                .111133.
+                ........
+            */
+
+            var badClaims = new HashSet<Claim>();
+            for (var column = 0; column < fabricWidth; column++)
+            {
+                for (var row = 0; row < fabricHeight; row++)
+                {
+                    if (grid[column, row].Count <= 1)
+                        continue;
+
+                    foreach (var claim in grid[column, row])
+                    {
+                        badClaims.Add(claim);
+                    }
+                }
+            }
+
+            foreach (var claim in claims)
+            {
+                if (badClaims.All(_ => _.Id != claim.Id))
+                {
+                    Console.WriteLine($"Claim: {claim.Id}");
+                }
+            }
         }
     }
 }
--- 2018/program.cs
+++ 2018/program.cs
@@ -9,7 +9,7 @@ namespace AoC._2018
         static void Main(string[] args)
         {
             const int day = 3;
-            const int part = 1;
+            const int part = 2;
 
             try
             {
