85a37b33ef1aacc977a56493cf65293635fae5fc
Mike McDermott <mike@mrm-inc.net>
2019-12-02 15:50:13 -0600

    Adding day 1

--- 2019/Program.cs
+++ 2019/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 
-namespace _2019
+namespace Aoc._2019
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string day = "1";
+            const int part = 1;
+
+            try
+            {
+                //Load puzzle class
+                var puzzle = (IPuzzle)Activator.CreateInstance(Assembly.GetExecutingAssembly().GetType($"AoC._2019.Day{day}.Part{part}"));
+
+                //Load puzzle input
+                puzzle.RawInput = File.ReadAllText($@"..\..\..\Day{day}\Part{part}Input.txt");
+
+                var stopwatch = Stopwatch.StartNew();
+                puzzle.Solve();
+                Console.WriteLine($"Solution complete in {stopwatch.ElapsedMilliseconds}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
         }
     }
 }
