bafd1a56abf453e1177c23116794da67d8bd6f68
mrmcderm <mrmcderm@gmail.com>
2020-12-11 06:26:56 -0600

    Day 10 Solution for just Part 1

---
+++ 2020/Day10/Part2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Aoc._2020.Day10
+{
+    public class Part2 : IPuzzle
+    {
+        public string RawInput { get; set; }
+
+        public void Solve()
+        {
+            var answer = 0;
+            var adapters = RawInput.Split(Environment.NewLine).Select(_ => int.Parse(_)).OrderBy(_ => _).ToList();
+            var oneJoltDiffs = 0;
+            var threeJoltDiffs = 1;
+
+            var currentAdapter = 0;
+
+            while (currentAdapter < adapters.Max())
+            {
+                var supportedAdapters = adapters.Where(_ => _ > currentAdapter && _ <= currentAdapter + 3).ToList();
+                var smallestSupportedAdapter = supportedAdapters.Min();
+
+                if (smallestSupportedAdapter - currentAdapter == 1)
+                {
+                    oneJoltDiffs++;
+                }
+                else
+                {
+                    threeJoltDiffs++;
+                }
+
+                currentAdapter = smallestSupportedAdapter;
+            }
+
+            answer = oneJoltDiffs * threeJoltDiffs;
+
+            Console.WriteLine("Day 10, Part 2");
+            Console.WriteLine($"Answer: {answer}");
+        }
+    }
+}
