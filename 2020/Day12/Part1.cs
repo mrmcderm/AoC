1810c24474582edc359bf453c6c4ba9a13226e05
mrmcderm <mrmcderm@gmail.com>
2020-12-12 00:42:09 -0600

    Day 12 Solutions

---
+++ 2020/Day12/Part1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace Aoc._2020.Day12
+{
+    public class Part1 : IPuzzle
+    {
+        public string RawInput { get; set; }
+
+        public void Solve()
+        {
+            var answer = 0;
+            var currentHeading = 90;
+            var x = 0;
+            var y = 0;
+
+            var instructions = RawInput.Split(Environment.NewLine).ToList();
+
+            foreach(var instruction in instructions)
+            {
+                var action = instruction[0];
+                var units = int.Parse(instruction[1..]);
+
+                switch(action)
+                {
+                    case 'N':
+                        y += units;
+                        break;
+                    case 'S':
+                        y -= units;
+                        break;
+                    case 'E':
+                        x += units;
+                        break;
+                    case 'W':
+                        x -= units;
+                        break;
+                    case 'L':
+                        currentHeading -= units;
+                        if (currentHeading < 0)
+                            currentHeading += 360;
+                        break;
+                    case 'R':
+                        currentHeading += units;
+                        if (currentHeading >= 360)
+                            currentHeading -= 360;
+                        break;
+                    case 'F':
+                        switch(currentHeading)
+                        {
+                            case 0:
+                                y += units;
+                                break;
+                            case 180:
+                                y -= units;
+                                break;
+                            case 90:
+                                x += units;
+                                break;
+                            case 270:
+                                x -= units;
+                                break;
+                        }
+                        break;
+                }
+
+                Console.WriteLine($"{instruction} ({(currentHeading == 0 ? "N" : currentHeading == 90 ? "E" : currentHeading == 180 ? "S" : "W")}) {x} {y}");
+            }
+
+            answer = Math.Abs(x) + Math.Abs(y);
+
+            Console.WriteLine("Day 12, Part 1");
+            Console.WriteLine($"Answer: {answer}");
+        }
+    }
+}
---
+++ 2020/Day12/Part2.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+
+namespace Aoc._2020.Day12
+{
+    public class Part2 : IPuzzle
+    {
+        public string RawInput { get; set; }
+        public int shipX = 0;
+        public int shipY = 0;
+        public int waypointX = 10;
+        public int waypointY = 1;
+
+        public void Solve()
+        {
+            var answer = 0;
+            var instructions = RawInput.Split(Environment.NewLine).ToList();
+
+            foreach (var instruction in instructions)
+            {
+                var action = instruction[0];
+                var units = int.Parse(instruction[1..]);
+
+                switch (action)
+                {
+                    case 'N':
+                        waypointY += units;
+                        break;
+                    case 'S':
+                        waypointY -= units;
+                        break;
+                    case 'E':
+                        waypointX += units;
+                        break;
+                    case 'W':
+                        waypointX -= units;
+                        break;
+                    case 'L':
+                        switch (units)
+                        {
+                            case 90:
+                                RotateLeft();
+                                break;
+                            case 180:
+                                RotateLeft();
+                                RotateLeft();
+                                break;
+                            case 270:
+                                RotateLeft();
+                                RotateLeft();
+                                RotateLeft();
+                                break;
+                        }
+                        break;
+                    case 'R':
+                        switch(units)
+                        {
+                            case 90:
+                                RotateRight();
+                                break;
+                            case 180:
+                                RotateRight();
+                                RotateRight();
+                                break;
+                            case 270:
+                                RotateRight();
+                                RotateRight();
+                                RotateRight();
+                                break;
+                        }
+                        break;
+                    case 'F':
+                        shipX += waypointX * units;
+                        shipY += waypointY * units;
+                        break;
+                }
+
+                Console.WriteLine($"{instruction} Ship: {shipX} {shipY} Waypoint: {waypointX} {waypointY}");
+            }
+
+            answer = Math.Abs(shipX) + Math.Abs(shipY);
+
+            Console.WriteLine("Day 12, Part 1");
+            Console.WriteLine($"Answer: {answer}");
+        }
+
+        private void RotateRight()
+        {
+            int newX;
+            int newY;
+
+            newX = waypointY;
+            newY = waypointX;
+            waypointX = newX;
+            waypointY = -newY;
+        }
+
+        private void RotateLeft()
+        {
+            int newX;
+            int newY;
+
+            newX = waypointY;
+            newY = waypointX;
+            waypointX = -newX;
+            waypointY = newY;
+        }
+    }
+}
