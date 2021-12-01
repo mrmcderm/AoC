005db758efd5a87b6b221fffc62b2e5fa1dae888
Mike McDermott <mmcdermott@microsoft.com>
2021-11-30 22:27:44 -0600

    Initial add of 2021 scafolding

---
+++ 2021/Day01/Part1.cs
@@ -0,0 +1,15 @@
+﻿namespace Aoc._2021.Day01
+{
+    public class Part1 : IPuzzle
+    {
+        public string? RawInput { get; set; }
+
+        public void Solve()
+        {
+            var answer = 0;
+
+            Console.WriteLine("Day 1, Part 1");
+            Console.WriteLine($"Answer: {answer}");
+        }
+    }
+}
---
+++ 2021/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Aoc._2021;
+
+const string day = "01";
+const int part = 1;
+
+try
+{
+    //Load puzzle class //Aoc._2021.Day01
+    var puzzleType = Assembly.GetExecutingAssembly().GetType($"Aoc._2021.Day{day}.Part{part}");
+    if (puzzleType == null)
+    {
+        throw new Exception($"Could not resolve puzzle for Day: {day}, Part: {part}");
+    }
+
+    var puzzle = (IPuzzle)Activator.CreateInstance(puzzleType);
+
+    //Load puzzle input
+    puzzle.RawInput = File.ReadAllText($@"/Users/mmcdermott/Source/Repos/Aoc/2020/Day{day}/Part{part}Input.txt");
+
+    var stopwatch = Stopwatch.StartNew();
+    puzzle.Solve();
+    Console.WriteLine($"Solution complete in {stopwatch.ElapsedMilliseconds}ms");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadLine();
