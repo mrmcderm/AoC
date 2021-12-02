7f616bedfc4e3ac5babff9ff0ff4c0e6c5daf1ad
Mike McDermott <mrmcderm@gmail.com>
2021-12-01 23:14:58 -0600

    Day 2 Solutions

---
+++ 2021/Day02/Part1.cs
@@ -0,0 +1,37 @@
+﻿namespace Aoc._2021.Day02
+{
+    public class Part1 : IPuzzle
+    {
+        public string? RawInput { get; set; }
+
+        public void Solve()
+        {
+            var input = RawInput.Split(Environment.NewLine).ToList().Select(_ => _.Split(' ')).ToList();
+            var position = 0;
+            var depth = 0;
+
+            foreach(var item in input)
+            {
+                switch(item[0])
+                {
+                    case "forward":
+                        position += int.Parse(item[1]);
+                        break;
+                    case "down":
+                        depth += int.Parse(item[1]);
+                        break;
+                    case "up":
+                        depth -= int.Parse(item[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            var answer = position * depth;
+
+            Console.WriteLine("Day 2, Part 1");
+            Console.WriteLine($"Answer: {answer}");
+        }
+    }
+}
---
+++ 2021/Day02/Part2.cs
@@ -0,0 +1,40 @@
+﻿namespace Aoc._2021.Day02
+{
+    internal class Part2 : IPuzzle
+    {
+        public string? RawInput { get; set; }
+
+        public void Solve()
+        {
+            var input = RawInput.Split(Environment.NewLine).ToList().Select(_ => _.Split(' ')).ToList();
+            var position = 0;
+            var depth = 0;
+            var aim = 0;
+
+            foreach (var item in input)
+            {
+                switch (item[0])
+                {
+                    case "forward":
+                        var unit = int.Parse(item[1]);
+                        position += unit;
+                        depth += aim * unit;
+                        break;
+                    case "down":
+                        aim += int.Parse(item[1]);
+                        break;
+                    case "up":
+                        aim -= int.Parse(item[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            var answer = position * depth;
+
+            Console.WriteLine("Day 2, Part 2");
+            Console.WriteLine($"Answer: {answer}");
+        }
+    }
+}
