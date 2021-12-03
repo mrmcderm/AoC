a6f6ce7ab3e8be9396508f17c1e58a07f931cb79
Mike McDermott <mrmcderm@gmail.com>
2021-12-02 23:56:10 -0600

    Part 1 Solved.  Part 2 in progress.

---
+++ 2021/Day03/Part1.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+
+namespace Aoc._2021.Day03
+{
+    public class Part1 : IPuzzle
+    {
+        public string? RawInput { get; set; }
+
+        public void Solve()
+        {
+            var answer = 0;
+            var diagnosticNumbers = RawInput.Split(Environment.NewLine).Select(_ => _.ToArray()).ToList();
+
+            var bitLength = diagnosticNumbers[0].Length;
+            var bitCounts = new int[bitLength];
+            var gammaArray = new BitArray(bitLength);
+            var epsilonArray = new BitArray(bitLength);
+
+            foreach (var diagnosticNumber in diagnosticNumbers)
+            {
+                for(int i = 0; i < bitLength; i++)
+                {
+                    if(diagnosticNumber[i] == '1')
+                    {
+                        bitCounts[i]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < bitLength; i++)
+            {
+                var bit = bitCounts[i] > diagnosticNumbers.Count / 2;
+                gammaArray.Set(i, bit);
+                epsilonArray.Set(i, !bit);
+            }
+
+            Reverse(gammaArray);
+            int[] array = new int[1];
+            gammaArray.CopyTo(array, 0);
+            var gamma = array[0];
+
+            Reverse(epsilonArray);
+            array = new int[1];
+            epsilonArray.CopyTo(array, 0);
+            var epsilon = array[0];
+
+            answer = gamma * epsilon;
+
+            Console.WriteLine("Day 3, Part 1");
+            Console.WriteLine($"Answer: {answer}");
+        }
+
+        public void Reverse(BitArray array)
+        {
+            int length = array.Length;
+            int mid = (length / 2);
+
+            for (int i = 0; i < mid; i++)
+            {
+                bool bit = array[i];
+                array[i] = array[length - i - 1];
+                array[length - i - 1] = bit;
+            }
+        }
+    }
+}
---
+++ 2021/Day03/Part2.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+
+namespace Aoc._2021.Day03
+{
+    internal class Part2 : IPuzzle
+    {
+        public string? RawInput { get; set; }
+
+        public void Solve()
+        {
+            var answer = 0;
+            var diagnosticNumbers = RawInput.Split(Environment.NewLine).Select(_ => _.ToArray()).ToList();
+
+            var bitLength = diagnosticNumbers[0].Length;
+            var bitCounts = new int[bitLength];
+
+            foreach (var diagnosticNumber in diagnosticNumbers)
+            {
+                for (int i = 0; i < bitLength; i++)
+                {
+                    if (diagnosticNumber[i] == '1')
+                    {
+                        bitCounts[i]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < bitLength; i++)
+            {
+                if (diagnosticNumbers.Count > 1)
+                {
+                    var bit = bitCounts[i] > diagnosticNumbers.Count / 2;
+
+                    var newDiagnosticNumbers = new List<char[]>();
+
+                    foreach (var diagnosticNumber in diagnosticNumbers)
+                    {
+                        if ((bit && diagnosticNumber[i] == '1') || !bit && diagnosticNumber[i] == '0')
+                        {
+                            newDiagnosticNumbers.Add(diagnosticNumber);
+                        }
+                    }
+
+                    diagnosticNumbers = newDiagnosticNumbers;
+                }
+            }
+
+
+
+            Console.WriteLine("Day 3, Part 2");
+            Console.WriteLine($"Answer: {answer}");
+        }
+
+        public void Reverse(BitArray array)
+        {
+            int length = array.Length;
+            int mid = (length / 2);
+
+            for (int i = 0; i < mid; i++)
+            {
+                bool bit = array[i];
+                array[i] = array[length - i - 1];
+                array[length - i - 1] = bit;
+            }
+        }
+    }
+}
