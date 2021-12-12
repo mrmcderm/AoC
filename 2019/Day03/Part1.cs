4d529f5ecbd886cf1b24c503c95de7ade0182e82
Mike McDermott <mrmcderm@gmail.com>
2021-12-12 12:09:34 -0600

    2019 continued work

--- 2019/Day03/Part1.cs
+++ 2019/Day03/Part1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc._2019.Day03
 {
@@ -20,14 +21,58 @@ namespace Aoc._2019.Day03
             {
                 var direction = wireInstruction[0];
                 var length = int.Parse(wireInstruction[1..]);
+                var gridPoint = (0, 0);
 
                 switch(direction)
                 {
                     case 'R':
-                        for(var i = 0; i < length; i++)
+
+                        for(var i = 0; i <= length; i++)
                         {
-                            var gridPoint = (endOfWire1.Item1 + i, endOfWire1.Item2);
+                            gridPoint = (endOfWire1.Item1 + i, endOfWire1.Item2);
                             if(gridPoints.ContainsKey(gridPoint))
+                            {
+                                gridPoints[gridPoint]++;
+                            }
+                            else
+                            {
+                                gridPoints.Add(gridPoint, 1);
+                            }
+                        }
+                        break;
+                    case 'L':
+                        for (var i = 0; i <= length; i++)
+                        {
+                            gridPoint = (endOfWire1.Item1 - i, endOfWire1.Item2);
+                            if (gridPoints.ContainsKey(gridPoint))
+                            {
+                                gridPoints[gridPoint]++;
+                            }
+                            else
+                            {
+                                gridPoints.Add(gridPoint, 1);
+                            }
+                        }
+                        break;
+                    case 'U':
+                        for (var i = 0; i <= length; i++)
+                        {
+                            gridPoint = (endOfWire1.Item1, endOfWire1.Item2 + i);
+                            if (gridPoints.ContainsKey(gridPoint))
+                            {
+                                gridPoints[gridPoint]++;
+                            }
+                            else
+                            {
+                                gridPoints.Add(gridPoint, 1);
+                            }
+                        }
+                        break;
+                    case 'D':
+                        for (var i = 0; i <= length; i++)
+                        {
+                            gridPoint = (endOfWire1.Item1, endOfWire1.Item2 - i);
+                            if (gridPoints.ContainsKey(gridPoint))
                             {
                                 gridPoints[gridPoint]++;
                             }
@@ -39,15 +84,16 @@ namespace Aoc._2019.Day03
                         break;
                     default:
                         break;
-
                 }
+
+                endOfWire1 = gridPoint;
             }
+
+            answer = gridPoints.Where(_ => _.Value > 1).Select(_ => CalcualteManhattanDistance((0,0), _.Key)).Min();
 
             Console.WriteLine("Day 3, Part 1");
             Console.WriteLine($"Answer: {answer}");
         }
-
-        private void BuildWire(string[] wireInstructions)
 
         private int CalcualteManhattanDistance((int, int) coord1, (int, int) coord2)
         {
