51dee36af78ebb6d0d3a27e4ece7961d642a02f7
Mike McDermott <mrmcderm@gmail.com>
2022-12-06 05:32:36 -0600

    Day 6 Complete.

---
+++ 2022/Day06/Part1.cs
@@ -0,0 +1,27 @@
+﻿namespace Aoc._2022.Day06
+{
+    public class Part1 : IPuzzle
+    {
+        public string? RawInput { get; set; }
+
+        public void Solve()
+        {
+            var answer = 0;
+            var dataStream = RawInput;
+            var packetSize = 4;
+
+            for(var i = 0; i < dataStream.Length - packetSize; i++)
+            {
+                var nextSequence = dataStream.Skip(i).Take(packetSize).ToList();
+                if(nextSequence.Distinct().Count() >= packetSize)
+                {
+                    answer = i += packetSize;
+                    break;
+                }
+            }
+            
+            Console.WriteLine("Day 6, Part 1");
+            Console.WriteLine($"Answer: {answer}");
+        }
+    }
+}
---
+++ 2022/Day06/Part2.cs
@@ -0,0 +1,27 @@
+﻿namespace Aoc._2022.Day06
+{
+    internal class Part2 : IPuzzle
+    {
+        public string? RawInput { get; set; }
+
+        public void Solve()
+        {
+            var answer = 0;
+            var dataStream = RawInput;
+            var packetSize = 14;
+
+            for (var i = 0; i < dataStream.Length - packetSize; i++)
+            {
+                var nextSequence = dataStream.Skip(i).Take(packetSize).ToList();
+                if (nextSequence.Distinct().Count() >= packetSize)
+                {
+                    answer = i += packetSize;
+                    break;
+                }
+            }
+
+            Console.WriteLine("Day 6, Part 2");
+            Console.WriteLine($"Answer: {answer}");
+        }
+    }
+}
