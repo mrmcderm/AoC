d7e670a86a5921e622326f697505055f40dba042
Mike McDermott <mrmcderm@gmail.com>
2023-12-03 17:14:30 -0600

    Day 2 Complete

--- 2023/Day02/Part1.cs
+++ 2023/Day02/Part1.cs
@@ -1,4 +1,6 @@
-﻿namespace Aoc._2023.Day02
+﻿using System.Drawing;
+
+namespace Aoc._2023.Day02
 {
     public class Part1 : IPuzzle
     {
@@ -6,7 +8,64 @@
 
         public void Solve()
         {
-            var answer = 0;
+            var games = RawInput.Split("\r\n");
+            var validGames = new List<int>();
+
+            foreach (var game in games)
+            {
+                var isValid = true;
+                var id = int.Parse(game.Split(":")[0].Split(" ")[1]);
+
+                var subsets = game.Split(": ")[1].Split("; ");
+
+                foreach (var subset in subsets)
+                {
+                    var colorCounts = subset.Split(", ");
+                    foreach (var colorCount in colorCounts)
+                    {
+                        var values = colorCount.Split(" ");
+                        var color = values[1];
+                        var count = int.Parse(values[0]);
+                        switch (color)
+                        {
+                            case "blue":
+                                if (count > 14)
+                                {
+                                    isValid = false;
+                                    continue;
+                                }                                
+                                break;
+
+                            case "red":
+                                if (count > 12)
+                                {
+                                    isValid = false;
+                                    continue;
+                                }
+                                break;
+
+                            case "green":
+                                if (count > 13)
+                                {
+                                    isValid = false;
+                                    continue;
+                                }
+                                break;
+
+                            default:
+                                break;
+
+                        }
+                    }
+                }
+
+                if (isValid)
+                {
+                    validGames.Add(id);
+                }
+            }
+
+            var answer = validGames.Sum();
 
             Console.WriteLine("Day 2, Part 1");
             Console.WriteLine($"Answer: {answer}");
--- 2023/Day02/Part2.cs
+++ 2023/Day02/Part2.cs
@@ -7,6 +7,44 @@
         public void Solve()
         {
             var answer = 0;
+            var games = RawInput.Split("\r\n");
+
+            foreach (var game in games)
+            {
+                var id = int.Parse(game.Split(":")[0].Split(" ")[1]);
+                var subsets = game.Split(": ")[1].Split("; ");
+                var maxRed = 0;
+                var maxBlue = 0;
+                var maxGreen = 0;
+
+                foreach (var subset in subsets)
+                {
+                    var colorCounts = subset.Split(", ");
+                    foreach (var colorCount in colorCounts)
+                    {
+                        var values = colorCount.Split(" ");
+                        var color = values[1];
+                        var count = int.Parse(values[0]);
+                        switch (color)
+                        {
+                            case "red":
+                                if (count > maxRed)
+                                    maxRed = count;
+                                break;
+                            case "blue":
+                                if (count > maxBlue)
+                                    maxBlue = count;
+                                break;
+                            case "green":
+                                if (count > maxGreen)
+                                    maxGreen = count;
+                                break;
+                        }
+                    }
+                }
+
+                answer += maxRed * maxBlue * maxGreen;
+            }
 
             Console.WriteLine("Day 2, Part 2");
             Console.WriteLine($"Answer: {answer}");
